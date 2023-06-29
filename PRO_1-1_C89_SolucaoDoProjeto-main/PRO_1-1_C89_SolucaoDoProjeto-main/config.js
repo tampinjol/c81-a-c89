588839664fc37471f11ca9c3cd63c44df3544b47
tampinjol <105882492+tampinjol@users.noreply.github.com>
2023-06-29 16:01:54 -0300

    Add files via upload

---
+++ PRO_1-1_C89_SolucaoDoProjeto-main/PRO_1-1_C89_SolucaoDoProjeto-main/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBLeQdLzpFYiNVf4ASJ7pyFGv0xAzQ18go",
+  authDomain: "projeto-89-9016b.firebaseapp.com",
+  databaseURL: "https://projeto-89-9016b-default-rtdb.firebaseio.com",
+  projectId: "projeto-89-9016b",
+  storageBucket: "projeto-89-9016b.appspot.com",
+  messagingSenderId: "956142688781",
+  appId: "1:956142688781:web:e925f6786a7662c9071344",
+  measurementId: "G-0CFXCXLBFS"
+};
